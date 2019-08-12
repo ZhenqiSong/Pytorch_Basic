86ca6555df6479dd234406765408fd6e70e8abc1
ZhenqiSong <songzhenqi92@gmail.com>
2019-08-12 21:53:58 +0800

    Tensor基础

--- 60Minutes/1_what_is_pytorch.py
+++ 60Minutes/1_what_is_pytorch.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import torch
+import numpy as np
 
 # 创建一个随机未初始化的空tensor
 x = torch.empty(5, 3)
@@ -55,7 +56,6 @@ a.add_(1)
 print(a)
 print(b)
 
-import numpy as np
 a = np.ones(5)
 b = torch.from_numpy(a)
 np.add(a, 1, out=a)
